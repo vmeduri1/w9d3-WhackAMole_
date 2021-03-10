60d919aac60cc45f4f3dd7a666fab64332da3557
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-03-10 15:25:14 -0600

    Moles

--- mole.js
+++ mole.js
@@ -1,10 +1,10 @@
 function popUpRandomMole() {
-    let moleheads = document.querySelectorAll(".wgs__mole-head");
-    
+    let moleheads = document.querySelectorAll(".wgs__mole-head:not(.wgs__mole-head--whacked)");
+
     let i = Math.floor(Math.random() * Math.floor(7));
     moleheads[i].classList.remove("wgs__mole-head--hidden");
-    console.log(moleheads[i]);
-    setTimeout(() => hideMole(moleheads[i]), 1000);
+    console.log(moleheads);
+    setTimeout(() => hideMole(moleheads[i]), 3000);
 }
 
 function hideMole(element) {
@@ -16,5 +16,11 @@ function hideMole(element) {
 window.addEventListener("DOMContentLoaded", () => {
 
     setTimeout(popUpRandomMole, 0);
-
+    let moleheads = document.querySelectorAll(".wgs__mole-head");
+    moleheads.forEach((mole) => {
+        mole.addEventListener("click", () => {
+            mole.classList.add("wgs__mole-head--hidden");
+            mole.classList.add("wgs__mole-head--whacked");
+        })
+    })
 })
